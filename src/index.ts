776c477a84514148612ee7bfd805a8613eef2d01
Millerz50 <johwanisi1@gmail.com>
2025-10-22 13:19:32 +0200

    env and twirks

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ app.use(compression());
 //
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://treasure-pal-p24x.vercel.app/",
+  "https://treasure-pal.vercel.app",
 ];
 
 const corsOptions = {
