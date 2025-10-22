2a78069f1b984be252aa119f4c60c55c4715632f
Millerz50 <johwanisi1@gmail.com>
2025-10-22 15:38:56 +0200

    env and twirks

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCCx4dXBxCmMKt3J_u6ZnDyDDdnsymNbwI",
   authDomain: "treasurepal-5a1e4.firebaseapp.com",
   projectId: "treasurepal-5a1e4",
-  storageBucket: "treasurepal-5a1e4.appspot.com",
+  storageBucket: "treasurepal-5a1e4.firebasestorage.app",
   messagingSenderId: "1081098285699",
   appId: "1:1081098285699:web:2c148e170bd073011ba1b6",
   measurementId: "G-BSKBPWXBVH",
